d2eb195fd9957fa07b2d6fcef2a3e20812f3dd56
61-6D-6D-6F <30551234+61-6D-6D-6F@users.noreply.github.com>
2025-05-12 15:06:22 +0200

    feat: fetch top stories concurrently

--- main.go
+++ main.go
@@ -4,10 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 func main() {
-	fmt.Println(fetchTopIds())
+	topIds := fetchTopIds()
+	// fmt.Println(topIds)
+
+	ch := make(chan Story)
+	var wg sync.WaitGroup
+
+	for i, id := range topIds {
+		rank := i + 1
+		wg.Add(1)
+		go fetchStory(id, rank, ch, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for story := range ch {
+		fmt.Printf("%d. %s\n", story.Rank, story.Title)
+	}
 }
 
 func fetchTopIds() []int {
@@ -22,15 +42,46 @@ func fetchTopIds() []int {
 
 	defer res.Body.Close()
 
-	if res.StatusCode != 200 {
-		fmt.Println("Hacker News not available")
-		return topIds
-	}
-
 	if err := json.NewDecoder(res.Body).Decode(&topIds); err != nil {
 		fmt.Println("Error decoding ids of top stories response")
 		return topIds
 	}
 
 	return topIds
+}
+
+type Story struct {
+	Rank        int
+	By          string `json:"by"`
+	Title       string `json:"title"`
+	Url         string `json:"url"`
+	Text        string `json:"text"`
+	Kids        []int  `json:"kids"`
+	Descendants int    `json:"descendants"`
+}
+
+func fetchStory(id int, rank int, ch chan<- Story, wg *sync.WaitGroup) {
+	var story Story
+
+	story.Rank = rank
+
+	defer wg.Done()
+
+	url := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", id)
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error fetching story id: %d", id)
+		return
+	}
+
+	defer res.Body.Close()
+
+	if err := json.NewDecoder(res.Body).Decode(&story); err != nil {
+		fmt.Printf("Error decoding story id: %d", id)
+		return
+	}
+
+	ch <- story
+
+	return
 }
