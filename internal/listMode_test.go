7dc31c1395763bd07fd6367931579a5f837b5940
ammo <ammo@ammo.codes>
2025-05-18 18:20:57 +0200

    refactor: tests

--- internal/listMode_test.go
+++ internal/listMode_test.go
@@ -4,118 +4,91 @@ import (
 	"testing"
 )
 
+func testListChangeState(t *testing.T, input string, states ...[]State) {
+	for _, state := range states {
+		listMode := NewListMode(state[0])
+
+		newState := listMode.ChangeState(input)
+
+		if !equalState(newState, state[1]) {
+			t.Errorf("ChangeState(%s)\ninitial state:\n%+v\nnew state:\n%+v\nexpected state:\n%+v",
+				input, state[0], newState, state[1])
+		}
+	}
+}
+
 // TestListChangeStateNext calls internal.ChangeState with input "n",
 // checking for a valid state
 func TestListChangeStateNext(t *testing.T) {
 	input := "n"
-	expPageNumber := 1
 
-	var state State
+	testListChangeState(t, input,
+		// [ initial state,
+		//   expected state ]
 
-	listMode := NewListMode(state)
-
-	newState := listMode.ChangeState(input)
-
-	if newState.PageNumber != expPageNumber {
-		t.Errorf("ChangeState(n) = %d want to match %d", newState.PageNumber, expPageNumber)
-	}
-}
-
-// TestListChangeStateNextOnLast calls internal.ChangeState with input "n"
-// on the last item, checking for a valid state
-func TestListChangeStateNextOnLast(t *testing.T) {
-	input := "n"
-	expPageNumber := MAX_STORIES / NUM_PER_PAGE
-
-	var state State
-	state.PageNumber = MAX_STORIES / NUM_PER_PAGE
-
-	listMode := NewListMode(state)
-
-	newState := listMode.ChangeState(input)
-
-	if newState.PageNumber != expPageNumber {
-		t.Errorf("ChangeState(n) = %d want to match %d", newState.PageNumber, expPageNumber)
-	}
+		// not last page
+		[]State{
+			{PageNumber: 1},
+			{PageNumber: 2}},
+		// last page
+		[]State{
+			{PageNumber: MAX_STORIES / NUM_PER_PAGE},
+			{PageNumber: MAX_STORIES / NUM_PER_PAGE}},
+	)
 }
 
 // TestListChangeStatePrev calls internal.ChangeState with input "p",
 // checking for a valid state
 func TestListChangeStatePrev(t *testing.T) {
 	input := "p"
-	expPageNumber := 2
 
-	var state State
-	state.PageNumber = 3
+	testListChangeState(t, input,
+		// [ initial state,
+		//   expected state ]
 
-	listMode := NewListMode(state)
-
-	newState := listMode.ChangeState(input)
-
-	if newState.PageNumber != expPageNumber {
-		t.Errorf("ChangeState(p) = %d want to match %d", newState.PageNumber, expPageNumber)
-	}
+		// not first page
+		[]State{
+			{PageNumber: 2},
+			{PageNumber: 1}},
+		// first page
+		[]State{
+			{PageNumber: 1},
+			{PageNumber: 1}},
+	)
 }
 
-// TestListChangeStatePrevOnFirst calls internal.ChangeState with input "p"
-// on the first item, checking for a valid state
-func TestListChangeStatePrevOnFirst(t *testing.T) {
-	input := "p"
-	expPageNumber := 1
-
-	var state State
-	state.PageNumber = 1
-
-	listMode := NewListMode(state)
-
-	newState := listMode.ChangeState(input)
-
-	if newState.PageNumber != expPageNumber {
-		t.Errorf("ChangeState(p) = %d want to match %d", newState.PageNumber, expPageNumber)
-	}
-}
-
-// TestListChangeStateNum calls internal.ChangeState with input "3",
+// TestListChangeStateNum calls internal.ChangeState with input "2",
 // checking for a valid state
 func TestListChangeStateNum(t *testing.T) {
-	input := "3"
-	expStory := Story{Title: "expPageNumber"}
-	expMode := Details
+	input := "2"
 
-	var state State
-	state.FetchedStories = []Story{{}, {}, {Title: "expPageNumber"}}
+	testListChangeState(t, input,
+		// [ initial state,
+		//   expected state ]
 
-	listMode := NewListMode(state)
-
-	newState := listMode.ChangeState(input)
-
-	if newState.SelectedStory.Title != expStory.Title || newState.Mode != expMode {
-		t.Errorf("ChangeState(3) = %s, %d want to match %s, %d",
-			newState.SelectedStory.Title, newState.Mode,
-			expStory.Title, expMode)
-	}
+		// number 1-9
+		[]State{
+			{Mode: List, SelectedStory: Story{},
+				FetchedStories: []Story{{}, {Title: "exp"}}},
+			{Mode: Details, SelectedStory: Story{Title: "exp"},
+				FetchedStories: []Story{{}, {Title: "exp"}}}},
+	)
 }
 
 // TestListChangeStateNotSupported calls internal.ChangeState with input not supported,
 // checking for a valid state
 func TestListChangeStateNotSupported(t *testing.T) {
 	inputs := []string{"i", "44", "6f", "k9"}
-	expPageNumber := 22
-	expMode := List
-
-	var state State
-	state.PageNumber = 22
-	state.Mode = List
-
-	listMode := NewListMode(state)
 
 	for _, input := range inputs {
-		newState := listMode.ChangeState(input)
+		testListChangeState(t, input,
+			// [ initial state,
+			//   expected state ]
 
-		if newState.PageNumber != expPageNumber || newState.Mode != expMode {
-			t.Errorf("ChangeState(not supported) = %d, %d want to match %d, %d",
-				newState.PageNumber, newState.Mode,
-				expPageNumber, expMode)
-		}
+			// not supported
+			[]State{
+				{Mode: List, PageNumber: 2},
+				{Mode: List, PageNumber: 2}},
+		)
 	}
 }
